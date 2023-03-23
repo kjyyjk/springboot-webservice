be15ec57dfcdd7dc614cab4af15dd99d8b966d34
kjyyjk <rlawnsdud920@gmail.com>
2023-03-24 01:43:51 +0900

    JPA 테스트 코드 작성

---
+++ src/main/java/kjyyjk920/springboot/domain/posts/Posts.java
@@ -0,0 +1,39 @@
+package kjyyjk920.springboot.domain.posts;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    //id 애노테이션은 해당 테이블의 pk필드. 중복x 널값x. 행을 식별할 수 있는.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //해당  @Entity 클래스의 필드는 자동으로 칼럼이 된다.
+    //@Column 사용 이유 : 변경이 필요한 경우
+
+    //VARCHAR의 사이즈를 500으로 늘리려고 @Column
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    //TEXT로 타입 변경하려고
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title=title;
+        this.content=content;
+        this.author=author;
+    }
+}
