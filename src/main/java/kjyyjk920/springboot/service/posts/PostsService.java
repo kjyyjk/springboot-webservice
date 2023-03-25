71414432d69d34638fd7a5994aefa8bf7dd4e928
kjyyjk <rlawnsdud920@gmail.com>
2023-03-25 20:13:49 +0900

    Service, Controller - 등록API / 테스트

---
+++ src/main/java/kjyyjk920/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package kjyyjk920.springboot.service.posts;
+
+import kjyyjk920.springboot.domain.posts.PostsRepository;
+import kjyyjk920.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    public final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ src/main/java/kjyyjk920/springboot/web/PostsApiController.java
@@ -0,0 +1,23 @@
+package kjyyjk920.springboot.web;
+
+import kjyyjk920.springboot.service.posts.PostsService;
+import kjyyjk920.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
+
+
+
+
