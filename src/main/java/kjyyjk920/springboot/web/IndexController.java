66a5a9d2d0d2e9e101a64d497cf7d582dc1ce8b6
kjyyjk <rlawnsdud920@gmail.com>
2023-03-30 00:47:26 +0900

    posts 등록 버튼 및 등록 화면 개발

--- src/main/java/kjyyjk920/springboot/web/IndexController.java
+++ src/main/java/kjyyjk920/springboot/web/IndexController.java
@@ -2,6 +2,7 @@ package kjyyjk920.springboot.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -9,5 +10,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
